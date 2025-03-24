d963e2d61246d81c5e385284aaf1b5cca5407f9d
collidingScopes <shadesDrawnMusic@gmail.com>
2025-03-24 10:54:10 -0400

    update 3D model link for portal

--- portals.js
+++ portals.js
@@ -68,7 +68,7 @@ const portalDestinations = [
 ];
 let numPortalsPerLevel = 2;
 let urlParams = "?portal=true&username=panda&color=red&speed=5&ref=https://collidingscopes.github.io/red-panda-vibes/"
-urlParams += "&avatar_url=http://collidingscopes.github.io/red-panda-vibes/assets/panda3DModel6.glb";
+urlParams += "&avatar_url=http://collidingscopes.github.io/red-panda-vibes/assets/panda3DModel7.glb";
 
 function initPortalSystem() {
     console.log("Waiting for game initialization...");
